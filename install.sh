f845f3a1af8cde33991c3614628f7df21562e816
Caim <mirandakaio483@gmail.com>
2025-07-23 20:10:15 -0300

    feat: Initial dotfiles setup

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+FILES_TO_SYMLINK=(
+  'zshrc'
+  'gitconfig'
+)
+
+echo "Iniciando configuração dos dotfiles..."
+
+for file in "${FILES_TO_SYMLINK[@]}"; do
+  source_file="$DIR/$file"
+  destination_file="$HOME/.$file"
+
+  if [ -e "$destination_file" ]; then
+    echo "Fazendo backup de $destination_file para $destination_file.bak"
+    mv "$destination_file" "$destination_file.bak"
+  fi
+
+  echo "Criando link para $file em $destination_file"
+  ln -s "$source_file" "$destination_file"
+done
+
+echo "Configuração concluída!"
